0d65ba749215cdf5631f0d0d92124afed146f661
mbarancewicz <44418145+mbarancewicz@users.noreply.github.com>
2021-11-12 18:56:39 +0100

    added something

--- kolofortuny/fortuna.js
+++ kolofortuny/fortuna.js
@@ -1,3 +1,25 @@
+var game;
+
+function getRandomWord() {
+  return data[getRandomInt(0, data.length - 1)].country.toUpperCase();
+}
+
+function initGameState() {
+  var word = getRandomWord();
+  game = {
+    word: word,
+    lifes: 5,
+    blanks: word.length
+  };
+}
+
+function initGame() {
+  initGameState();
+  console.log(game.word);
+  displayGuessedWord();
+  document.getElementById("lifes").innerHTML = `Życia: ${game.lifes}`;
+}
+
 function openAuthorInfo() {
   document.getElementById("menu").style.display = 'none';
   document.getElementById("author").style.display = 'block';
@@ -11,12 +33,11 @@ function closeAuthorInfo() {
 function openGame() {
   document.getElementById("menu").style.display = 'none';
   document.getElementById("game").style.display = 'block';
-  displayGuessedWord();
+  initGame();
 }
 
 function displayGuessedWord() {
-  var word = "Poland";
-  for(const letter in word) {
+  for(const letter in game.word) {
     var letterDiv = document.createElement("div");
     letterDiv.className = "letter";
     letterDiv.id = `letter_${letter}`
@@ -27,24 +48,32 @@ function displayGuessedWord() {
 }
 
 function guessLetter() {
-  var word = "Paland".toUpperCase();
   var letter = document.getElementById("inputLetter").value.toUpperCase();
-  if(word.includes(letter)) {
-    for(var i = 0; i < word.length; i++) {
-      if(word[i] == letter) {
+  if(game.word.includes(letter)) {
+    for(var i = 0; i < game.word.length; i++) {
+      if(game.word[i] == letter) {
         document.getElementById(`letter_${i}`).innerHTML = letter;
       }
     }
+    game.blanks -= 1;
+    if(game.blanks === 0) {
+      clearLetters();
+      initGame();
+    }
   } else {
     var lifesDiv = document.getElementById("lifes");
-    var lifes = lifesDiv.innerText.split(' ')[1] - 1;
-    
-    if(lifes === 0) gameOver();
-    else lifesDiv.innerHTML = `Życia: ${lifes}`
+    game.lifes -= 1;
+    if(game.lifes === 0) gameOver();
+    else lifesDiv.innerHTML = `Życia: ${game.lifes}`
   }
 }
 
+function clearLetters() {
+  document.querySelectorAll('.letter').forEach(e => e.remove());
+}
+
 function gameOver() {
+  clearLetters();
   document.getElementById("game").style.display = 'none';
   document.getElementById("menu").style.display = 'block';
   document.getElementById("gameOver").style.display = 'block';
@@ -55,3 +84,4 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+// to do reset po wygranej
